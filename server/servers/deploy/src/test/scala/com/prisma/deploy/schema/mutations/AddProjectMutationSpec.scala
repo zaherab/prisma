a7998d9aff44c5eac6d3a55c41d20cf00f1fc756
Marcus Böhm <wif.mboehm@gmx.de>
2018-04-24 12:57:35 +0200

    move to right package

--- server/servers/deploy/src/test/scala/com/prisma/deploy/schema/mutations/AddProjectMutationSpec.scala
+++ server/servers/deploy/src/test/scala/com/prisma/deploy/schema/mutations/AddProjectMutationSpec.scala
@@ -1,4 +1,4 @@
-package com.prisma.deploy.database.schema.mutations
+package com.prisma.deploy.schema.mutations
 
 import com.prisma.deploy.specutils.DeploySpecBase
 import com.prisma.shared.models.ProjectId
